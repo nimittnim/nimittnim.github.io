76d7d53e3b646fbd66689ed20d25915761f61853
nimittnim <nimittnim@gmail.com>
2026-08-01 15:03:18 +0530

    update

---
+++ personal/script.js
@@ -0,0 +1,36 @@
+const tooltip = document.getElementById("tooltip");
+
+document.querySelectorAll(".day").forEach(day => {
+
+    if (!day.dataset.date) return;
+
+    day.addEventListener("mouseenter", () => {
+
+        tooltip.innerHTML = `
+            <strong>${day.dataset.date}</strong><br><br>
+
+            <b>${day.dataset.title}</b><br>
+
+            Distance: ${day.dataset.distance} km<br>
+            Time: ${day.dataset.duration}<br>
+            Pace: ${day.dataset.pace}/km
+
+            ${day.dataset.notes
+                ? `<hr>${day.dataset.notes}`
+                : ""}
+        `;
+
+        tooltip.style.display = "block";
+    });
+
+    day.addEventListener("mousemove", e => {
+
+        tooltip.style.left = (e.pageX + 15) + "px";
+        tooltip.style.top = (e.pageY + 15) + "px";
+    });
+
+    day.addEventListener("mouseleave", () => {
+        tooltip.style.display = "none";
+    });
+
+});
